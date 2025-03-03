fc45fd0d4c9c7c25294299ca73861dad96749961
Juan Cabrera <yesuscabrera2003@gmail.com>
2025-03-03 12:46:08 -0600

    Add multi_lines

--- PDF_template/main.py
+++ PDF_template/main.py
@@ -14,10 +14,16 @@ for index, row in data.iterrows():
     pdf.set_text_color(100, 100, 100)
     #arguments w=width,ln=breakline h=size same number
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
-    pdf.line(10, 20, 200, 20)
+
+    # Generar múltiples líneas horizontales en el PDF
+    # Se dibujan desde x=10 hasta x=200, comenzando en y=20 hasta y=298,
+    # con un espaciado de 10 unidades entre cada línea.
+    for y in range(20 , 298, 10): # Itera valores de "y" desde 20 hasta 298 con pasos de 10
+        pdf.line(10, y, 200, y)  # Dibuja una línea horizontal desde (10, y) hasta (200, y)
+    
 
     #set the footer
-    pdf.ln(260)
+    pdf.ln(265)
     pdf.set_font(family="Times", style="I", size=8)
     pdf.set_text_color(180, 180, 180)
     pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
@@ -30,5 +36,8 @@ for index, row in data.iterrows():
         pdf.set_font(family="Times", style="I", size=8)
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
+        for y in range(20 , 298, 10):
+            pdf.line(10, y, 200, y)
        
 pdf.output("output.pdf")
